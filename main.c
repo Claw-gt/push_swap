88cd3e947246c5d9c78620a8d2c69ec3a9a06c19
Claudia Garcia Talavera <clagarci@c1r8s4.42madrid.com>
2024-07-01 18:54:26 +0200

    parsing ko

--- main.c
+++ main.c
@@ -6,28 +6,17 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:10:25 by clagarci          #+#    #+#             */
-/*   Updated: 2024/07/01 17:26:00 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/07/01 18:41:43 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
-
-int to_number(int argc, char **argv)
-{
-	int i;
-
-	i = 0;
-	while (i++ < argc)
-	{
-		ft_atoi(argv[i]);
-	}
-	return (0);
-}
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	if (argc > 1) //el número de args debe ser mayor que 1, pue el primero es el ejecutable
+	if (argc > 1 && argc <= 100) //el número de args debe ser mayor que 1, pues el primero es el ejecutable
 	{
 		to_number(argc, argv);		
 		//check_duplicates(argv);
--- parsing.c
+++ parsing.c
@@ -6,23 +6,31 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:25:57 by clagarci          #+#    #+#             */
-/*   Updated: 2024/07/01 17:22:22 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/07/01 18:38:55 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdio.h>
 //int	check_spaces(char **argv)
 
-int to_number(int argc, char **argv)
+int *to_number(int argc, char **argv)
 {
 	int i;
+    int j;
+    int *num_array;
 
+    j = 0;
 	i = 0;
+    // num_array = (int *)malloc(sizeof(int) * (argc - 1));
+    // if (num_array == NULL)
+    //     return (NULL);
 	while (i++ < argc)
 	{
-		ft_atoi(argv[i]);
+        write(1, argv[i], sizeof(argv[i]));
+		num_array[j] = ft_atoi(argv[i]);
 	}
-	return (0);
+	return (num_array);
 }
 
 int check_duplicates()

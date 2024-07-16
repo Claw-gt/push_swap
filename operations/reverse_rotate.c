c6f89facd0b36a76af695cf80a7d30ac7007d6e9
Claudia <clagarci@student.42madrid.com>
2024-07-16 19:24:09 +0200

    added reverse rotate

--- operations/reverse_rotate.c
+++ operations/reverse_rotate.c
@@ -6,12 +6,50 @@
 /*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 20:47:06 by clagarci          #+#    #+#             */
-/*   Updated: 2024/07/15 17:07:11 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/07/16 19:23:35 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //rra
+void rra(t_stack **stack_a)
+{
+    t_stack *tmp;
+    t_stack *last;
 
+    if (*stack_a == NULL || (*stack_a)->next == NULL)
+        return ;
+    last = ft_lstlast(*stack_a);
+    tmp = *stack_a;
+    while (tmp->next != last)
+        tmp = tmp->next;
+    tmp->next = NULL;
+    last->next = *stack_a;
+    *stack_a = last;
+    write(1, "rra\n", 4);
+}
 //rrb
+void rrb(t_stack **stack_b)
+{
+    t_stack *tmp;
+    t_stack *last;
 
+    if (*stack_b == NULL || (*stack_b)->next == NULL)
+        return ;
+    last = ft_lstlast(*stack_b);
+    tmp = *stack_b;
+    while (tmp->next != last)
+        tmp = tmp->next;
+    tmp->next = NULL;
+    last->next = *stack_b;
+    *stack_b = last;
+    write(1, "rrb\n", 4);
+}
 //rrr
+void rrr(t_stack **stack_a, t_stack **stack_b)
+{
+    if (*stack_a == NULL || (*stack_a)->next == NULL || *stack_b == NULL || (*stack_b)->next == NULL)
+        return ;
+    rra(stack_a);
+    rrb(stack_b);
+    write(1, "rrr\n", 4);
+}

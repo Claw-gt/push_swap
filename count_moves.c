1878ced6c5dfe584d4c167de92dfb5c18147d317
Claudia Garcia Talavera <clagarci@c1r10s5.42madrid.com>
2024-08-07 16:48:55 +0200

    init count moves

--- count_moves.c
+++ count_moves.c
@@ -6,7 +6,32 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 12:11:42 by clagarci          #+#    #+#             */
-/*   Updated: 2024/08/07 13:33:40 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/08/07 16:48:32 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int count_moves(int number)
+{
+    int moves;
+
+    moves = 0;
+
+    return (moves);
+}
+
+void find_optim(t_stack **stack_b)
+{
+    t_stack *tmp;
+    int     cheapest;
+	int		min;
+
+    tmp = *stack_b;
+    cheapest = (*stack_b)->index;
+	min = count_moves(*stack_b)->index;
+    while (tmp)
+    {
+        if (count_moves(tmp->index) < min)
+			cheapest = tmp->index;
+        tmp = tmp->next;
+    }
+}

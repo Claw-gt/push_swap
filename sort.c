0ca97a4653fec9df867cb0167ff2cdf190d0798b
Claudia <clagarci@student.42madrid.com>
2024-08-08 16:18:21 +0200

    modified location of check_sorted

--- sort.c
+++ sort.c
@@ -6,11 +6,25 @@
 /*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 15:59:35 by clagarci          #+#    #+#             */
-/*   Updated: 2024/08/08 16:03:38 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/08/08 16:17:49 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int check_sorted(t_stack *stack)
+{
+    t_stack *tmp;
+
+    tmp = stack;
+    while (tmp->next)
+    {
+        if (tmp->index > tmp->next->index)
+            return (-1);
+        tmp = tmp->next;
+    }
+    return (0);
+}
 
 void sort_stack(t_stack **stack_a)
 {
--- utils.c
+++ utils.c
@@ -3,25 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 16:12:25 by clagarci          #+#    #+#             */
-/*   Updated: 2024/08/07 15:56:40 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/08/08 16:17:41 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int check_sorted(t_stack *stack)
-{
-    t_stack *tmp;
-
-    tmp = stack;
-    while (tmp->next)
-    {
-        if (tmp->index > tmp->next->index)
-            return (-1);
-        tmp = tmp->next;
-    }
-    return (0);
-}

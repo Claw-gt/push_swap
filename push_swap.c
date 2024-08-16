d7b25dc76d5a1531f2fa71b702b5e93bd9bcd07a
Claudia Garcia Talavera <clagarci@c1r12s6.42madrid.com>
2024-08-16 18:25:16 +0200

    checked null input

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:10:25 by clagarci          #+#    #+#             */
-/*   Updated: 2024/08/16 15:12:42 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/08/16 18:24:39 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,37 +19,47 @@ void	leaks(void){
 t_stack *process_stack(t_stack **stack_a, int *initial_array, int arguments)
 {
 	int i;
-	t_stack *temp;
+	//t_stack *temp;
 
 	i = 0;
-	temp = *stack_a;
+	//temp = *stack_a;
 	while (i < arguments)
 	{
 		ft_stckadd_back(stack_a, ft_stcknew(initial_array[i]));
 		i++;
 	}
-	while(temp)
-	{
-		printf("temp->content: %d\n", temp->content);
-		temp = temp->next;
-	}
+	// while(temp)
+	// {
+	// 	printf("temp->content: %d\n", temp->content);
+	// 	temp = temp->next;
+	// }
 	quick_sort(initial_array, 0, arguments - 1);
 	stack_indexing(stack_a, initial_array);
-	t_stack *check = *stack_a;
-	while(check)
-	{
-		printf("check->content: %d\n", check->content);
-		printf("check->index: %d\n", check->index);
-		check = check->next;
-	}
+	// t_stack *check = *stack_a;
+	// while(check)
+	// {
+	// 	printf("check->content: %d\n", check->content);
+	// 	printf("check->index: %d\n", check->index);
+	// 	check = check->next;
+	// }
 	return (*stack_a);
 }
 
 int	initial_check(int argc, char **argv)
 {
 	int	total_args;
+	int	i;
 
+	i = 0;
 	total_args = 0;
+	while (i++ < argc)
+	{
+		if (!argv[i])
+		{
+			print_errors();
+			return (-1);
+		}
+	}
 	total_args = count_and_check(argc - 1, argv);
 	if (argc < 2 || total_args == -1)
 	{	
@@ -71,12 +81,12 @@ int	main(int argc, char **argv)
 	initial_array = parse_input(arguments, argc, argv);
 	if (initial_array == NULL)
 		return (1);
-	int i = 0;
-	while (i < arguments)
-	{
-		printf("\ninitial_array[%d]: %d\n", i, initial_array[i]);
-		i++;
-	}
+	// int i = 0;
+	// while (i < arguments)
+	// {
+	// 	printf("\ninitial_array[%d]: %d\n", i, initial_array[i]);
+	// 	i++;
+	// }
 	stack_a = NULL;
 	stack_a = process_stack(&stack_a, initial_array, arguments);
 	free (initial_array);
@@ -84,6 +94,6 @@ int	main(int argc, char **argv)
 	//test_sort(&stack_a);
 	//test_sort_three(&stack_a);
 	//test_operations(&stack_a);
-	atexit (leaks);
+	//atexit (leaks);
 	return (0);
 }

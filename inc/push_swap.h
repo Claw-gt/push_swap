ab04083c53983fbf0851f650886e8cd23a63a4ff
Claudia Garcia Talavera <clagarci@c1r5s1.42madrid.com>
2024-08-19 18:00:02 +0200

    norminetted pass

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: clagarci <clagarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:17:30 by clagarci          #+#    #+#             */
-/*   Updated: 2024/08/19 16:22:20 by clagarci         ###   ########.fr       */
+/*   Updated: 2024/08/19 17:30:40 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,37 +28,33 @@ typedef struct s_stack
 	struct s_stack	*next;
 }					t_stack;
 
-int stack_position(t_stack **stack, int number);
+int		stack_position(t_stack **stack, int number);
 
-int target_position(t_stack **stack_a, int stack_b_index);
+int		target_position(t_stack **stack_a, int stack_b_index);
 
-int	to_number(char **split_array, int num_args, int **int_array, int position);
+int		initial_check(int argc, char **argv);
 
-int	count_and_check(int num_args, char **argv);
+int		count_and_check(int num_args, char **argv);
 
-int	find_midpoint(int size_stack);
+int		find_max(t_stack *stack);
 
-int find_max(t_stack *stack);
+void	push_to_b(t_stack **stack_a);
 
-int find_min(t_stack *stack);
-
-void push_to_b(t_stack **stack_a);
-
-void sort_stack(t_stack **stack_a);
+void	sort_stack(t_stack **stack_a);
 
 void	*print_errors(void);
 
-int	*parse_input (int arguments, int argc, char *argv[]);
+int		*parse_input(int arguments, int argc, char *argv[]);
 
-int	check_duplicates(int *array, int arguments);
+int		check_duplicates(int *array, int arguments);
 
-int partition(int *array, int low, int high);
+int		partition(int *array, int low, int high);
 
-void quick_sort(int *array, int low, int high);
+void	quick_sort(int *array, int low, int high);
 
-int find_number(int *array, int number);
+int		find_number(int *array, int number);
 
-void stack_indexing(t_stack **stack, int *array);
+void	stack_indexing(t_stack **stack, int *array);
 
 void	ft_stckadd_back(t_stack **stck, t_stack *new);
 
@@ -68,49 +64,43 @@ t_stack	*ft_stcklast(t_stack *stck);
 
 void	ft_stckclear(t_stack **stck);
 
-int	ft_stcksize(t_stack *stck);
+int		ft_stcksize(t_stack *stck);
 
-void sa(t_stack **stack_a);
+void	sa(t_stack **stack_a, int duplicate);
 
-void sb(t_stack **stack_b);
+void	sb(t_stack **stack_b, int duplicate);
 
-void ss(t_stack **stack_a, t_stack **stack_b);
+void	ss(t_stack **stack_a, t_stack **stack_b);
 
-void pa(t_stack **stack_a, t_stack **stack_b);
+void	pa(t_stack **stack_a, t_stack **stack_b);
 
-void pb(t_stack **stack_a, t_stack **stack_b);
+void	pb(t_stack **stack_a, t_stack **stack_b);
 
-void ra(t_stack **stack_a, int duplicate);
+void	ra(t_stack **stack_a, int duplicate);
 
-void rb(t_stack **stack_b, int duplicate);
+void	rb(t_stack **stack_b, int duplicate);
 
-void rr(t_stack **stack_a, t_stack **stack_b);
+void	rr(t_stack **stack_a, t_stack **stack_b);
 
-void rra(t_stack **stack_a, int duplicate);
+void	rra(t_stack **stack_a, int duplicate);
 
-void rrb(t_stack **stack_b, int duplicate);
+void	rrb(t_stack **stack_b, int duplicate);
 
-void rrr(t_stack **stack_a, t_stack **stack_b);
+void	rrr(t_stack **stack_a, t_stack **stack_b);
 
-void test_operations(t_stack **stack);
+void	sort_three(t_stack **stack_a);
 
-void test_sort_three(t_stack **stack);
+void	sort_more(t_stack **stack_a);
 
-void test_sort(t_stack **stack);
+void	sort_stack(t_stack **stack_a);
 
-void sort_three(t_stack **stack_a);
+int		check_sorted(t_stack *stack);
 
-void sort_more(t_stack **stack_a);
+int		count_moves(int position, int size_stack);
 
-void sort_stack(t_stack **stack_a);
+t_stack	*find_optim(int init, int target, t_stack **stack_b, t_stack **stack_a);
 
-int optimization(t_stack **stack);
+void	moves(t_stack *optim_node, t_stack **stack_a, t_stack **stack_b);
 
-int check_sorted(t_stack *stack);
-
-int count_moves(int position, int size_stack);
-
-t_stack	*find_optim(int initial, int target, t_stack **stack_b, t_stack **stack_a);
-
-void moves(t_stack *optim_node, t_stack **stack_a, t_stack **stack_b);
+void	*free_array(char **arr, int num);
 #endif
